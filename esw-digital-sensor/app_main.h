b7d102afd0b61ad74f60b3370029c00ced4a1cbb
ruts9 <ruts9@github.com>
2022-03-30 23:44:13 +0300

    Probleemne

---
+++ esw-digital-sensor/app_main.h
@@ -0,0 +1,13 @@
+/**
+ * @file app_main.h
+ * 
+ * @author Johannes Ehala, ProLab.
+ * @license MIT
+ *
+ * Copyright ProLab, TTÜ. 2021
+ */
+
+#ifndef ACCEL_APP_MAIN_H_
+#define ACCEL_APP_MAIN_H_
+
+#endif // ACCEL_APP_MAIN_H_
---
+++ esw-digital-sensor/build/tsb0/home/rudolf/node-apps/zoo/lammertb.libcrc/src/crcccitt.d
@@ -0,0 +1,3 @@
+build/tsb0//home/rudolf/node-apps/zoo/lammertb.libcrc/src/crcccitt.o: \
+ /home/rudolf/node-apps/zoo/lammertb.libcrc/src/crcccitt.c \
+ /home/rudolf/node-apps/zoo/lammertb.libcrc/include/checksum.h
